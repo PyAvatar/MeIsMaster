34778d9608c23268ad48e516139f6884f1b61114
PyAvatar <106009618+PyAvatar@users.noreply.github.com>
2022-11-09 00:28:25 +0100

    Create HelloKotlin.kt

---
+++ HelloKotlin.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Hello World!");
+    println("Hello Hell");
+    
+    var A = 5;
+    var B = 6;
+    var C = A + B;
+    
+    println(A);
+    println(B);
+    println(C);
+    
+}
