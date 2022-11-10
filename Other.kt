7eacecb679e9dcb68527662803fcb75afc9d6467
PyAvatar <106009618+PyAvatar@users.noreply.github.com>
2022-11-10 18:54:40 +0100

    Create Other.kt

---
+++ Other.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("Hello World!");
+    println("Hello Hell");
+    
+    var A = 5;
+    var B = 6;
+    var C = A + B;
+    
+    println(A);
+    println(B);
+    println(C);
+    
+    println(A);
+    println(B);
+    println(C);
+}
